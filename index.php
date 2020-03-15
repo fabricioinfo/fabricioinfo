76d928e3dbe3d3ed4d491ff9d3af9bdd0dbddb09
fabricioinfo <fabricio1989@gmail.com>
2020-03-15 17:07:23 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html class="no-js" lang="pt-br">
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="css/modelo.css">
+    <link rel="stylesheet" type="text/css" href="css/all.css">
+	<title>Fabrício Info Tutoria Online</title>
+	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+<script src="js/all.js"></script>
+</head>
+<body>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-12">
+        <div class="navbar">
+          <div class="nav-brand">
+            <h3>Fabrício Info</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-lg-3 col-xl-3 col-12">
+          <div class="card">
+              <div class="face face1">
+                  <div class="content">
+                    <h3><i class="fas fa-user-graduate fa-4x"></i></h3>
+                    <h3>Aulas EAD Antigo</h3>
+                  </div>
+              </div> 
+              <div class="face face2">
+                  <div class="content">
+                      <p>
+                        Plataforma online para que os alunos do professor Fabrício possam acessar os materiais, vídeo aula e outros itens que auxiliam nos estudos.
+                      </p>
+                      <a href="http://fabricioinfo.com.br/moodle" target="_blank">Clique aqui</a>
+                  </div>
+              </div>   
+          </div>
+      </div>
+
+      <div class="col-lg-3 col-xl-3 col-12">
+          <div class="card">
+              <div class="face face1">
+                  <div class="content">
+                    <h3><i class="fas fa-user-graduate fa-4x"></i></h3>
+                    <h3>Aulas EAD Novo</h3>
+                  </div>
+              </div> 
+              <div class="face face2">
+                  <div class="content">
+                      <p>
+                        Plataforma online para que os alunos do professor Fabrício possam acessar os materiais, vídeo aula e outros itens que auxiliam nos estudos.
+                      </p>
+                      <a href="https://classroom.google.com/" target="_blank">Clique aqui</a>
+                  </div>
+              </div>   
+          </div>
+      </div>
+
+      <div class="col-lg-3 col-xl-3 col-12">
+          <div class="card">
+              <div class="face face1">
+                  <div class="content">
+                    <h3><i class="fas fa-chalkboard-teacher fa-4x"></i></h3>
+                    <h3>Sobre o Fabrício</h3>
+                  </div>
+              </div> 
+              <div class="face face2">
+                  <div class="content">
+                      <p>
+                        Saiba mais sobre o professor Fabrício e sua formação clicando no botão abaixo.
+                      </p>
+                      <a href="http://lattes.cnpq.br/2374845553959025" target="_blank">Clique aqui</a>
+                  </div>
+              </div>   
+          </div>
+      </div>
+
+      <div class="col-lg-3 col-xl-3 col-12">
+          <div class="card">
+              <div class="face face1">
+                  <div class="content">
+                    <h3><i class="fab fa-youtube fa-4x"></i></h3>
+                    <h3>Canal do Youtube</h3>
+                  </div>
+              </div> 
+              <div class="face face2">
+                  <div class="content">
+                      <p>
+                        Conheça o canal do Professor Fabrício no youtube e se inscreva, curta e compartilhe os vídeos que você mais gostou, transmita o conhecimento.
+                      </p>
+                      <a href="https://www.youtube.com/channel/UC1Raw_0hA7QqHlOhOzoZ0Hw" target="_blank">Clique aqui</a>
+                  </div>
+              </div>   
+          </div>
+      </div>
+
+    </div>
+  </div>
+</body>
+</html>
