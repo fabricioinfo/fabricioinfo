d9779fb3367e115852bb3f40551876fe40ecfd5e
fabricioinfo <fabricio1989@gmail.com>
2020-03-16 12:05:52 -0300

    Create hotlink.php

---
+++ hotlink.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html class="no-js" lang="pt-br">
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="css/modelo.css">
+    <link rel="stylesheet" type="text/css" href="css/all.css">
+	<title>Fabrício Info Tutoria Online</title>
+	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+<script src="js/all.js"></script>
+</head>
+<body>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-12">
+        <div class="navbar hotmart">
+          <div class="nav-brand">
+            <h3>Cursos Onlines Hotmart</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-lg-3 col-xl-3 col-12">
+          <div class="card">
+              <div class="face face1 hotmart">
+                  <div class="content">
+                    <h3><i class="fas fa-user-graduate fa-4x"></i></h3>
+                    <h3>Orientação Para TCC</h3>
+                  </div>
+              </div> 
+              <div class="face face2">
+                  <div class="content">
+                      <p>
+                        Aprenda a configurar os estilos do Word para as normas da ABNT. Configure referências e nunca mais fique perdido dentre tantas citações e outras configurações.
+                      </p>
+                      <a href="https://go.hotmart.com/Q23770499I?dp=1" target="_blank">Clique aqui</a>
+                  </div>
+              </div>   
+          </div>
+      </div>
+
+      <div class="col-lg-3 col-xl-3 col-12">
+          <div class="card">
+              <div class="face face1 hotmart">
+                  <div class="content">
+                    <h3><i class="fas fa-spa fa-4x"></i></h3>
+                    <h3>Curso de Maquiagem</h3>
+                  </div>
+              </div> 
+              <div class="face face2">
+                  <div class="content">
+                      <p>
+                         Curso Maquiagem na Web é totalmente Online, disponível por 1 ano, 24 horas por dia. São mais de 35 vídeo aulas+ atualizações de novas aulas + certificado.
+                      </p>
+                      <a href="https://go.hotmart.com/K23245044T?dp=1" target="_blank">Clique aqui</a>
+                  </div>
+              </div>   
+          </div>
+      </div>
+
+      <div class="col-lg-3 col-xl-3 col-12">
+          <div class="card">
+              <div class="face face1 hotmart">
+                  <div class="content">
+                    <h3><i class="fas fa-pen-nib fa-4x"></i></h3>
+                    <h3>Curso de Desenho (Anime)</h3>
+                  </div>
+              </div> 
+              <div class="face face2">
+                  <div class="content">
+                      <p>
+                        MÉTODO EXCLUSIVO VÍDEO AULAS 100% PASSO A PASSO E DO ZERO! MÓDULO COMPLETO DE COLORIR! MÉTODO INFÁLIVEL APRENDA A DESENHAR DE TUDO
+                      </p>
+                      <a href="https://go.hotmart.com/P19404241F?ap=d6b6" target="_blank">Clique aqui</a>
+                  </div>
+              </div>   
+          </div>
+      </div>
+
+      <div class="col-lg-3 col-xl-3 col-12">
+          <div class="card">
+              <div class="face face1 hotmart">
+                  <div class="content">
+                    <h3><i class="fas fa-egg fa-4x"></i></h3>
+                    <h3>Curso Ovos de Páscoa Gourmet</h3>
+                  </div>
+              </div> 
+              <div class="face face2">
+                  <div class="content">
+                      <p>
+                        O Curso Ovos de Páscoa Gourmet 2.0 está completo, com receitas incríveis , fáceis de fazer e descomplicadas para você lucrar muito nessa Páscoa!
+                      </p>
+                      <a href="https://go.hotmart.com/P23773121N?ap=254e" target="_blank">Clique aqui</a>
+                  </div>
+              </div>   
+          </div>
+      </div>
+
+    </div>
+
+    
+
+  </div>
+</body>
+</html>
